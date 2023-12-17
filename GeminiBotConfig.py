c30161be8411801b7d1d6559410a850f9b0a5dcb
reminmg2000 <153965329+reminmg2000@users.noreply.github.com>
2023-12-17 12:01:42 +0800

    修改配置参数

--- GeminiBotConfig.py
+++ GeminiBotConfig.py
@@ -1,6 +1,6 @@
 ﻿#Create at https://makersuite.google.com/
-GOOGLE_AI_KEY = "GOOGLE_AI_KEY"
+GOOGLE_AI_KEY = "AIzaSyBXRKZQInzqSXS3NLEVGCpLGknzcii4bM0"
 #Create at https://discord.com/developers/applications
-DISCORD_BOT_TOKEN = "DISCORD_BOT_KEY"
+DISCORD_BOT_TOKEN = "MTE4NTM5NzMxNjM2Mjk2NTExMw.GMx7fh.Q23inyv6dqUW1F67U-Gw8lFgqdaTZVlPcuAQpA"
 #Disable history by setting to 0
 MAX_HISTORY = 15
